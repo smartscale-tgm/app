fd2a46521037af92ca8e9830075d58305c20ea07
Benjamin Bulis <bbulis@student.tgm.ac.at>
2019-05-06 15:41:45 +0200

    Working on DatePicker for Register

--- app/src/main/java/com/example/smartscaleapp/RegisterActivity.java
+++ app/src/main/java/com/example/smartscaleapp/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.example.smartscaleapp;
 
+
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
